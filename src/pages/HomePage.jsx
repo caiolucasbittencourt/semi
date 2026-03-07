63c8a0d9ba709c2347efdcce3760995c8dd89c37
caiolucasbittencourt <caiolucasbittencourt@hotmail.com>
2026-03-07 16:55:31 -0300

    fix: improve anchor navigation between routes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import HeroSection from "../components/HeroSection";
 import SpecsSection from "../components/SpecsSection";
 import FeaturesSection from "../components/FeaturesSection";
@@ -5,6 +7,19 @@ import InteriorSection from "../components/InteriorSection";
 import CTASection from "../components/CTASection";
 
 export default function HomePage() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.state?.scrollTo) {
+      const element = document.getElementById(location.state.scrollTo);
+      if (element) {
+        setTimeout(() => {
+          element.scrollIntoView({ behavior: "smooth" });
+        }, 100);
+      }
+    }
+  }, [location.state]);
+
   return (
     <>
       <HeroSection />
